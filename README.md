cb869f03becd36c74d96c2a8744f8331fc3848d3
zacard.lin <13816025562@139.com>
2021-09-11 20:05:18 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# zmore
-A Personal Collection Bot
+# ZMore
+```text
+ZMore，纸墨收藏，根据wechaty消息来存储，复制收集指定类型文章，增强微信本身收藏能力
+```
+
---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['gitmoji'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
